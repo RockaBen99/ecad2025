09a74a43cb21ce489d56041f720d333f55296117
Benedict Orlich <benedict.orlich@gmail.com>
2025-10-14 12:22:46 +0100

    Complete updowncounter.sv

--- updown/updowncounter.sv
+++ updown/updowncounter.sv
@@ -1,12 +1,22 @@
 module updowncounter
   (
-   input logic        clk,
-   input logic        rst,
-   input logic        up,
-   output logic [3:0] count
-   );
+    input logic        clk,
+    input logic        rst,
+    input logic        up,
+    output logic [3:0] count
+  );
 
-   // insert your code here
-   assign count = 0;
+  // insert your code here
+  assign count = 0;
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst)
+      count <= 0;
+    else
+      if (up)
+        count <= count + 1;
+      else
+        count <= count - 1;
+  end
+   
 
 endmodule
