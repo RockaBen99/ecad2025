0375d37cfe30916e9b5c6bd77a8eb51a93b15858
Benedict Orlich <bo310@cam.ac.uk>
2025-11-15 22:40:39 +0000

    Implement div.s and test

--- riscv/assembly/src/div.s
+++ riscv/assembly/src/div.s
@@ -15,9 +15,57 @@ div:
 
     # do your work
     # example of printing inputs a0 and a1
-    DEBUG_PRINT a0
-    DEBUG_PRINT a1
+    # DEBUG_PRINT a0
+    # DEBUG_PRINT a1
 
+    beqz a1, divzero
+    blt a0, a1, divlt
+
+    li t0, 0 # Q := 0
+    li t1, 0 # R := 0
+
+    # get initial value of i = n - 1
+    li t2, 0 # i
+    mv t3, a0
+loop1:
+    beqz t3, end1
+    addi t2, t2, 1
+    srli t3, t3, 1
+    j loop1
+end1:
+    # i := n
+
+loop2:
+    addi t2, t2, -1 # i--
+    bltz t2, end2
+    slli t1, t1, 1 # R := R << 1
+    # N(i)
+    srl t3, a0, t2
+    andi t3, t3, 1
+
+    add t1, t1, t3 # R(0) := N(i)
+
+    blt t1, a1, loop2 # if R >= D
+    sub t1, t1, a1 # R := R - D
+    # Q(i) := 1
+    li t3, 1
+    sll t3, t3, t2
+    add t0, t0, t3
+    j loop2
+end2:
+    mv a0, t0
+    mv a1, t1
+    j end
+
+divzero:
+    li a0, 0
+    li a1, 0
+    j end
+
+divlt:
+    li a0, 0
+
+end:
     # load every register you stored above
     lw   ra, 28(sp)
     lw   s0, 24(sp)
